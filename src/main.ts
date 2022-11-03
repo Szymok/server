558f21a513a5b61cf91948e84303bf01f8bbe132
Szymon Kowalewski <szymonkowal019@gmail.com>
2022-11-03 19:13:14 +0100

    mongo db conn

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { VersioningType, ValidationPipe } from '@nestjs/common'
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'log', 'verbose'],
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('MasterClazz API')
@@ -16,6 +27,11 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   const PORT = process.env.PORT || 8080;
+
+  app.use(compression());
+  app.use(cookieParser());
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
   
   await app.listen(PORT).then(() => {
     console.log(`Server running on port ${PORT}`)
