5476179276354d19af45d89441da08b2bf29c103
Szymon Kowalewski <szymonkowal019@gmail.com>
2022-11-16 21:36:04 +0100

    create resource posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), 
-    MongooseModule.forRoot(process.env.DATABASE_URL),
+    MongooseModule.forRoot(process.env.DATABASE_URL), 
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
