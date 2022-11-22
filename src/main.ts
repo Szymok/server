914874412a31e224866667be14a45d8a9d8c72d2
Szymon Kowalewski <szymonkowal019@gmail.com>
2022-11-22 15:38:09 +0100

    mui

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
+    cors: {
+      origin: '*',
+    },
   });
 
   app.enableVersioning({
